c04bc2c1c15b47c33b7645ca12215bc5144ad1ed
Lucas Fernandes <tjslucasvl@gmail.com>
2025-03-29 07:21:10 -0300

    aut new update

--- produtividade/aut.js
+++ produtividade/aut.js
@@ -1,7 +1,7 @@
   // Verifica autenticação do usuário
     const username = localStorage.getItem('username');
     if (!username) {
-      window.location.href = '/WMS-Tools/login.html';
+      window.location.href = '/404.html';
     } else {
       document.getElementById('userNameDisplay').textContent = username;
     }
