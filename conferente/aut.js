8ffae1ce1a191f3aa635617860e64148b5f6ac05
Lucas Fernandes <tjslucasvl@gmail.com>
2025-05-04 07:10:06 -0300

    clear site

--- conferente/aut.js
+++
@@ -1,7 +0,0 @@
-  // Verifica autenticação do usuário
-    const username = localStorage.getItem('username');
-    if (!username) {
-      window.location.href = '/WMS-Tools/404.html';
-    } else {
-      document.getElementById('userNameDisplay').textContent = username;
-    }
