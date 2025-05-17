3987fd0b829d448f36a927c9eaeb85726bc0f337
Lucas Fernandes <tjslucasvl@gmail.com>
2025-05-17 20:38:59 -0300

    Update sync-sql.js

--- js/sync-sql.js
+++ js/sync-sql.js
@@ -340,8 +340,8 @@ window.sincronizarAgora = async () => {
 ////////////////////////////////////////////////////////////////////////////////
 
 // Define os intervalos mínimo e máximo
-const INTERVALO_MIN_MS = 15000;   // 15 segundos (rápido)
-const INTERVALO_MAX_MS = 120000;  // 2 minutos (econômico)
+const INTERVALO_MIN_MS = 30000;   // 30 segundos (rápido)
+const INTERVALO_MAX_MS = 300000;  // 5 minutos (econômico)
 
 let ultimoSync = 0;
 let intervaloAtual = INTERVALO_MIN_MS;
